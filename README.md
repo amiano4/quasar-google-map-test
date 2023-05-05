da41cf918f04b2eb3c450e31240a8d65bebeceb7
amiano4 <almariomiano@gmail.com>
2023-05-05 12:47:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quasar-google-map-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quasarframework-srmyga)
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page>
+    <div style="height: calc(100vh)">
+      <GMapMap :center="center" :zoom="10" map-type-id="terrain">
+        <GMapCluster :zoomOnClick="true">
+          <GMapMarker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center = m.position"
+          />
+        </GMapCluster>
+      </GMapMap>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+const center = { lat: 51.093048, lng: 6.84212 };
+const markers = [
+  {
+    position: {
+      lat: 51.093048,
+      lng: 6.84212,
+    },
+  },
+  {
+    position: {
+      lat: 51.198429,
+      lng: 6.69529,
+    },
+  },
+  {
+    position: {
+      lat: 51.165218,
+      lng: 7.067116,
+    },
+  },
+  {
+    position: {
+      lat: 51.09256,
+      lng: 6.84074,
+    },
+  },
+];
+</script>
